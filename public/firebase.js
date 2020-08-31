2eba09fb0539a34dd5e0a2ba2c5d4cf232700329
Mrinal Bedi <mrinalbedi42@gmail.com>
2020-08-31 11:32:51 -0400

    First commit

---
+++ public/firebase.js
@@ -0,0 +1,11 @@
+export default
+    {
+        apiKey: "AIzaSyCg8Yh82pescZiLSsnHlaYF7DqsfQF6UEY",
+        authDomain: "prog-8110-final-assignment.firebaseapp.com",
+        databaseURL: "https://prog-8110-final-assignment.firebaseio.com",
+        projectId: "prog-8110-final-assignment",
+        storageBucket: "prog-8110-final-assignment.appspot.com",
+        messagingSenderId: "265507541303",
+        appId: "1:265507541303:web:0ca6ff2bd4cb1bf62a8ab2",
+        measurementId: "G-7TWNHPMQWY"
+    };
